34ab21f1c05e2dfc1ff1926d5204dbd5787841a3
Jolonto <aluyapeter7@gmail.com>
2025-09-24 21:47:04 +0100

    health test

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Salon Finder API v0.1")
+}
 
 func main() {
-	fmt.Println("testing")
+	http.HandleFunc("/health", healthHandler)
+
+	fmt.Println("Starting server on :8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
